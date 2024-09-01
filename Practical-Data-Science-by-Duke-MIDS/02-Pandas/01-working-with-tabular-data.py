23e0644ac4aaf509b9bfb0cd328de464ccc94dea
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-09-01 13:14:45 +0100

    new file:   Practical-Data-Science-by-Duke-MIDS/02-Pandas/01-working-with-tabular-data.py

---
+++ Practical-Data-Science-by-Duke-MIDS/02-Pandas/01-working-with-tabular-data.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+
+#================================#
+# Working with tabular data #
+#================================#
+
+a = np.array([6.1,150.0,25,'A-'])
+b = np.array([5.6,122.0,29,'B+'])
+c = a + b
+
+# Pandas has no problem with mixed data types
+a = pd.Series([6.1, 150.0, 25, "A-"])
+b = pd.Series([5.6, 122.0, 29, "B+"])
+c = a + b
+print(c)
