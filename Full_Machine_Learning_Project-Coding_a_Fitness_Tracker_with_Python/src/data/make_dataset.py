8e6a15344eb4b406f6949e5287dce2f2d0faf53a
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-09-02 10:24:44 +0100

    learn: Merge the data into single dataframe

--- Full_Machine_Learning_Project-Coding_a_Fitness_Tracker_with_Python/src/data/make_dataset.py
+++ Full_Machine_Learning_Project-Coding_a_Fitness_Tracker_with_Python/src/data/make_dataset.py
@@ -182,6 +182,21 @@ acc_df, gyr_df = read_data_from_files(files)
 # Merging datasets
 # --------------------------------------------------------------
 
+data_merged = pd.concat([acc_df.iloc[:,:3], gyr_df], axis=1)
+
+data_merged.columns = [
+    "acc_x",
+    "acc_y",
+    "acc_z",
+    "gyr_x",
+    "gyr_y",
+    "gyr_z",
+    "label",
+    "category",
+    "participant",
+    "set"
+]
+
 
 # --------------------------------------------------------------
 # Resample data (frequency conversion)
