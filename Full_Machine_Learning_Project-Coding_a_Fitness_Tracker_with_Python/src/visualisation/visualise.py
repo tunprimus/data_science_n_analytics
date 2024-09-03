adf680b136aab3b18468645219abe2c5630bb8a8
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-09-03 13:23:37 +0100

    learn: Plot all the types of exercises

--- Full_Machine_Learning_Project-Coding_a_Fitness_Tracker_with_Python/src/visualisation/visualise.py
+++ Full_Machine_Learning_Project-Coding_a_Fitness_Tracker_with_Python/src/visualisation/visualise.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from os.path import realpath as realpath
+from IPython.display import display
 
 pd.set_option("mode.copy_on_write", True)
 
@@ -17,11 +18,38 @@ df = pd.read_pickle(real_path_to_pickle)
 # Plot single columns
 # --------------------------------------------------------------
 
+# Get a subset of the data
+set_df = df[df["set"] == 1]
+
+# Plot a sample column
+plt.plot(set_df["acc_y"])
+# Reset index from timestamp
+plt.plot(set_df["acc_y"].reset_index(drop=True))
 
 # --------------------------------------------------------------
 # Plot all exercises
 # --------------------------------------------------------------
 
+# Get all the types of exercises with .unique()
+exercise_labels = df["label"].unique()
+
+# Plot acc_y for each type of exercise
+for label in exercise_labels:
+    subset = df[df["label"] == label]
+    # display(subset.head(2))
+    fig, ax = plt.subplots()
+    plt.plot(subset["acc_y"].reset_index(drop=True), label=label)
+    plt.legend()
+    plt.show()
+
+# Plot 100 sample of acc_y for each type of exercise
+for label in exercise_labels:
+    subset = df[df["label"] == label]
+    # display(subset.head(2))
+    fig, ax = plt.subplots()
+    plt.plot(subset[:100]["acc_y"].reset_index(drop=True), label=label)
+    plt.legend()
+    plt.show()
 
 # --------------------------------------------------------------
 # Adjust plot settings
