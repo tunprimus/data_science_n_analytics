11e771f66b0ca966dbf1fcb36c307d22c196cb5a
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-09-03 17:33:52 +0100

    learn: Combine plots in one figure

--- Full_Machine_Learning_Project-Coding_a_Fitness_Tracker_with_Python/src/visualisation/visualise.py
+++ Full_Machine_Learning_Project-Coding_a_Fitness_Tracker_with_Python/src/visualisation/visualise.py
@@ -144,6 +144,21 @@ for label in exercise_labels:
 # Combine plots in one figure
 # --------------------------------------------------------------
 
+# Subset a label and participant into DataFrame
+label = "row"
+participant = "A"
+combined_plot_df = (df.query(f"label == '{label}'").query(f"participant == '{participant}'").reset_index(drop=True))
+
+# Split figure into subplots
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+# Plot each sensor data in a subplot
+combined_plot_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax[0])
+combined_plot_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+
+# Add some styling
+ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+ax[1].set_xlabel("samples")
 
 # --------------------------------------------------------------
 # Loop over all combinations and export for both sensors
