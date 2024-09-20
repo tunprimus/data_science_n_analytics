8b8468793aae12f4f3162af273aae70ac9207469
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-09-20 11:59:47 +0100

    learn: Add DataFrame summary functionality

--- Mastering_Exploratory_Data_Analysis/EDA-utilities.py
+++ Mastering_Exploratory_Data_Analysis/EDA-utilities.py
@@ -64,3 +64,85 @@ def column_summary(df):
 
     summary_df = pd.DataFrame(summary_data)
     return summary_df
+
+
+def column_summary_plus(df):
+    """
+    Creates a summary of a given DataFrame, including the following information:
+        - The column's data type
+        - The number of distinct values
+        - The minimum and maximum values
+        - The median value of non-null values
+        - The average value of non-null values
+        - The average value of non-zero values
+        - Whether null values are present
+        - The number of null values
+        - The number of non-null values
+        - A dictionary where the keys are the top 10 distinct values and the values are the counts of each distinct value.
+
+    Parameters
+    ----------
+    df : DataFrame
+        The DataFrame to be summarised.
+
+    Returns
+    -------
+    DataFrame
+        A DataFrame containing the summary of the given DataFrame.
+    """
+    result_df = pd.DataFrame(columns=["col_name", "col_dtype", "num_of_distinct_values", "min_value", "max_value", "median_no_na", "average_no_na", "average_non_zero", "null_present", "nulls_num", "non_nulls_num",  "distinct_values"])
+
+    df_columns = df.columns
+    # Loop through each column in the DataFrame
+    for column in df_columns:
+        print(f"Start processing {column} col with {df[column].dtype} dtype")
+        # Get column dtype
+        col_dtype = df[column].dtype
+        # Get distinct values and their counts
+        value_counts = df[column].value_counts()
+        distinct_values = value_counts.index.tolist()
+        # Get number of distinct values
+        num_distinct_values = len(distinct_values)
+        # Get min and max values
+        sorted_values = sorted(distinct_values)
+        min_value = sorted_values[0] if sorted_values else None
+        max_value = sorted_values[-1] if sorted_values else None
+
+        # Get median value
+        non_distinct_val_list = sorted(df[column].dropna().tolist())
+        len_non_d_list = len(non_distinct_val_list)
+        if len(non_distinct_val_list) == 0:
+            median = None
+        else:
+            median = non_distinct_val_list[len_non_d_list // 2]
+        
+        # Get average value if value is number
+        if np.issubdtype(df[column].dtype, np.number):
+            if len(non_distinct_val_list) > 0:
+                average = sum(non_distinct_val_list) / len_non_d_list
+                non_zero_val_list = [v for v in non_distinct_val_list if v > 0]
+                average_non_zero = sum(non_zero_val_list)/len_non_d_list
+            else:
+                average = None
+                average_non_zero = None
+        else:
+            average = None
+            average_non_zero = None
+
+        # Check if null values are present
+        null_present = 1 if df[column].isnull().any() else 0
+
+        # Get number of nulls and non-nulls
+        num_nulls = df[column].isnull().sum()
+        num_non_nulls = df[column].notnull().sum()
+
+        # Distinct_values only take top 10 distinct values count
+        top_10_d_v = value_counts.head(10).index.tolist()
+        top_10_c = value_counts.head(10).tolist()
+        top_10_d_v_dict = dict(zip(top_10_d_v, top_10_c))
+
+        # Append the information to the result DataFrame
+        result_df = result_df.append({"col_name": column, "col_dtype": col_dtype, "num_distinct_values": num_distinct_values, "min_value": min_value, "max_value": max_value, "median_no_na": median, "average_no_na": average, "average_non_zero": average_non_zero, "null_present": null_present, "nulls_num": num_nulls, "non_nulls_num": num_non_nulls, "distinct_values": top_10_d_v_dict})
+    
+    return result_df
+
