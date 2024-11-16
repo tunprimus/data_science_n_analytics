4da5bc161a9f224601000334433067fbd77d9e7b
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-11-16 16:18:22 +0100

    learn: Sort nested dictionaries

---
+++ Useful_data_processing_scripts/sort_nested_dictionary.py
@@ -0,0 +1,12 @@
+import json
+
+
+with open("nested_people.json", mode="r", encoding="utf-8") as read_file:
+    people = json.load(read_file)
+    print(people)
+
+    sort_by_age = dict(sorted(people.items(), key=lambda x: x[1]["age"], reverse=True))
+    print(sort_by_age)
+
+    sort_by_key = dict(sorted(people.items(), key=lambda x: x[0], reverse=True))
+    print(sort_by_key)
