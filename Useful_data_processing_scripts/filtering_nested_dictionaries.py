298b3a99900680ad2754a80ac4e17b82bcfef445
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-11-16 15:24:59 +0100

    learn: Filtering nested dictionary or json data

---
+++ Useful_data_processing_scripts/filtering_nested_dictionaries.py
@@ -0,0 +1,12 @@
+import json
+
+
+with open("nested_people.json", mode="r", encoding="utf-8") as read_file:
+    people = json.load(read_file)
+    print(people)
+
+    filter_by_age = dict(filter(lambda x: x[1]["age"] > 20, people.items()))
+    print(filter_by_age)
+
+    filter_by_key_length = dict(filter(lambda x: len(x[0]) == 5, people.items()))
+    print(filter_by_key_length)
