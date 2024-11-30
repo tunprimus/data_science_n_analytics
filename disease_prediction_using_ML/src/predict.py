98d4703cf1fd016c5b49f116c7632cdca825d1a8
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-11-30 22:37:18 +0100

    learn: Add starter files

---
+++ disease_prediction_using_ML/src/predict.py
@@ -0,0 +1,20 @@
+# Importing libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import mode
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+np.float = np.float64
+np.int = np.int_
+np.object = np.object_
+np.bool = np.bool_
+
+pd.set_option("mode.copy_on_write", True)
