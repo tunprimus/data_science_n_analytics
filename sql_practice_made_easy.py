0335a20d968ec6f5f9052ee71cf9d459ed702e7e
T Ol <31147296+tunprimus@users.noreply.github.com>
2025-02-04 16:20:23 +0100

    fix: Use context manager with sqlite

--- sql_practice_made_easy.py
+++ sql_practice_made_easy.py
@@ -6,24 +6,23 @@ from os.path import realpath as realpath
 pd.set_option("mode.copy_on_write", True)
 
 # Connect to the Chinook SQLite database
-conn = sqlite3.connect(realpath("../.assets/data/chinook.db"))
+with sqlite3.connect(realpath("../.assets/data/chinook.db")) as conn:
+    # Query to view all tables
+    tables_query = "SELECT name FROM sqlite_master WHERE type='table';"
+    tables = pd.read_sql(tables_query, conn)
+    print(tables)
 
-# Query to view all tables
-tables_query = "SELECT name FROM sqlite_master WHERE type='table';"
-tables = pd.read_sql(tables_query, conn)
-print(tables)
+    # Query the customers table
+    customers_query = "SELECT * FROM customers LIMIT 11;"
+    customers = pd.read_sql(customers_query, conn)
+    print(customers)
 
-# Query the customers table
-customers_query = "SELECT * FROM customers LIMIT 11;"
-customers = pd.read_sql(customers_query, conn)
-print(customers)
-
-# Join the Track and Album tables to get track names and album titles
-track_album_join_query = """
-SELECT tracks.Name AS tracks_name, albums.Title AS albums_title
-FROM tracks
-JOIN albums ON tracks.AlbumId = albums.AlbumId
-LIMIT 11;
-"""
-track_album = pd.read_sql(track_album_join_query, conn)
-print(track_album)
+    # Join the Track and Album tables to get track names and album titles
+    track_album_join_query = """
+    SELECT tracks.Name AS tracks_name, albums.Title AS albums_title
+    FROM tracks
+    JOIN albums ON tracks.AlbumId = albums.AlbumId
+    LIMIT 11;
+    """
+    track_album = pd.read_sql(track_album_join_query, conn)
+    print(track_album)
