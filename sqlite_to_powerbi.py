4bdd326a528d95c835a2d1551cd7d691633903e1
T Ol <31147296+tunprimus@users.noreply.github.com>
2025-02-04 16:23:21 +0100

    fix: Place code functionality within context manager

--- sqlite_to_powerbi.py
+++ sqlite_to_powerbi.py
@@ -6,31 +6,27 @@ from os.path import realpath as realpath
 pd.set_option("mode.copy_on_write", True)
 
 # Connect to the SQLite database
-conn = sqlite3.connect(realpath("../.assets/data/chinook.db"))
+with sqlite3.connect(realpath("../.assets/data/chinook.db")) as conn:
+    # Function to load all tables into a dictionary of DataFrames
+    def load_all_tables(conn):
+        tables = {}
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        table_names = cursor.fetchall()
 
-# Function to load all tables into a dictionary of DataFrames
-def load_all_tables(conn):
-    tables = {}
-    cursor = conn.cursor()
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    table_names = cursor.fetchall()
+        for table_name in table_names:
+            table_name = table_name[0]
+            tables[table_name] = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
 
-    for table_name in table_names:
-        table_name = table_name[0]
-        tables[table_name] = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
+        return tables
 
-    return tables
+    # Load all tables into a dictionary of DataFrames
+    tables = load_all_tables(conn)
 
-# Load all tables into a dictionary of DataFrames
-tables = load_all_tables(conn)
+    # Display the DataFrame of a specific table, e.g., "album"
+    print(tables["albums"].head())
 
-# Display the DataFrame of a specific table, e.g., "album"
-print(tables["albums"].head())
-
-# Loop over all tables and print them
-for idx, table_name in enumerate(tables):
-    print(table_name)
-    print(tables[f"{table_name}"].head())
-
-# Close the connection
-conn.close()
+    # Loop over all tables and print them
+    for idx, table_name in enumerate(tables):
+        print(table_name)
+        print(tables[f"{table_name}"].head())
