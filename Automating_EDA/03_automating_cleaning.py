03d2ea6c1e625f81dcbb0319b49fb582894e5de7
T Ol <31147296+tunprimus@users.noreply.github.com>
2025-03-06 13:35:46 +0100

    modified:   Automating_EDA/03_automating_cleaning.py

--- Automating_EDA/03_automating_cleaning.py
+++ Automating_EDA/03_automating_cleaning.py
@@ -62,10 +62,36 @@ def basic_wrangling(df, features=[], missing_threshold=0.95, unique_threshold=0.
 
 ### Date and Time Management
 
-def can_convert_to_datetime(df, col_list=[], messages=True):
+def can_convert_dataframe_to_datetime(df, col_list=[], return_result=True, messages=False):
+    """
+    Check if columns in a DataFrame can be converted to datetime format.
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        DataFrame to check
+    col_list : list, default []
+        List of columns to check. If empty, all columns with dtype of object will be checked.
+    return_result : bool, default True
+        If `True`, return a dictionary with column names as keys and boolean values indicating
+        whether each column can be converted to datetime format. If `False`, return nothing and print
+        messages.
+    messages : bool, default False
+        If `True`, print messages indicating whether each column can be converted to datetime format.
+
+    Returns
+    -------
+    result : dict or list
+        Dictionary with column names as keys and boolean values indicating
+        whether each column can be converted to datetime format, or list of boolean values.
+    """
     import pandas as pd
     import numpy as np
     from pandas.api.types import is_datetime64_any_dtype as is_datetime
+
+    # Change message flag based on whether or not to return result
+    if not return_result:
+        messages = True
 
     length_col_list = len(col_list)
     # Define type of return object
@@ -78,7 +104,7 @@ def can_convert_to_datetime(df, col_list=[], messages=True):
         columns_to_check = df.columns
     else:
         columns_to_check = df.columns[df.columns.isin(col_list)]
-    print(columns_to_check)
+        print(columns_to_check)
     # Check only columns with dtype of object
     for col in columns_to_check:
         # result = []
@@ -95,14 +121,62 @@ def can_convert_to_datetime(df, col_list=[], messages=True):
                     pass
             if messages:
                 print(f"Can convert {col} to datetime? {can_be_datetime}")
+            # Choose return data structure
             if length_col_list == 1:
                 result.append(can_be_datetime)
             else:
                 result[col] = can_be_datetime
-    return result
+    if return_result:
+        return result
+
+
+def can_convert_value_to_datetime(val, messages=False):
+    """
+    Check if a value can be converted to datetime format.
+
+    Parameters
+    ----------
+    val : value to check
+    messages : bool, default False
+        If `True`, print messages indicating whether the value can be converted to datetime format.
+
+    Returns
+    -------
+    can_be_datetime : bool
+        Whether the value can be converted to datetime format.
+    """
+    from pandas.api.types import is_datetime64_any_dtype as is_datetime
+    try:
+        df_dt_tmp = pd.to_datetime(val)
+        try:
+            df_flt_tmp = val.astype(np.float64)
+            can_be_datetime = False
+        except:
+            can_be_datetime = is_datetime(df_dt_tmp)
+    except:
+        can_be_datetime = False
+    if messages:
+        print(f"Can convert {val} to datetime? {can_be_datetime}")
+    return can_be_datetime
+
 
 
 def convert_to_datetime(df, messages=True):
+    """
+    Convert columns of object dtype to datetime, if possible.
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        DataFrame to check
+    messages : bool, default True
+        If `True`, print messages indicating which columns were converted to datetime format.
+
+    Returns
+    -------
+    df : pandas DataFrame
+        DataFrame with datetime columns, if possible
+    """
     import pandas as pd
     import numpy as np
     import sys
@@ -133,8 +207,9 @@ def parse_date(df, features=[], days_to_today=False, drop_date=True, messages=Tr
 
     all_cols = df.columns
     for feat in features:
-        if feat in all_cols and is_datetime(df[feat]):
+        if (feat in all_cols) and (can_convert_value_to_datetime(df[feat])):
             df[feat] = pd.to_datetime(df[feat])
+            df[f"{feat}_datetime"] = df[feat]
             df[f"{feat}_year"] = df[feat].dt.year
             df[f"{feat}_month"] = df[feat].dt.month
             df[f"{feat}_day"] = df[feat].dt.day
@@ -203,11 +278,19 @@ basic_wrangling(df_nba_salaries)
 basic_wrangling(df_airbnb)
 basic_wrangling(df_airline_satisfaction)
 
-can_convert_to_datetime(df_airbnb)
-can_convert_to_datetime(df_airbnb, col_list=["name"])
-can_convert_to_datetime(df_airbnb, col_list=["name", "last_review", "host_name"])
+can_convert_dataframe_to_datetime(df_airbnb)
+can_convert_dataframe_to_datetime(df_airbnb, col_list=["name"])
+can_convert_dataframe_to_datetime(df_airbnb, col_list=["name"])[0]
+can_convert_dataframe_to_datetime(df_airbnb, col_list=["last_review"])[0]
+can_convert_dataframe_to_datetime(df_airbnb, col_list=["name", "last_review", "host_name"])
+
+can_convert_value_to_datetime(df_airbnb["name"])
+can_convert_value_to_datetime(df_airbnb["last_review"])
+can_convert_value_to_datetime(df_airbnb["host_id"])
 
 convert_to_datetime(df_airbnb)
 
 parse_date(df_airbnb, features=["last_review"])
 parse_date(df_airbnb, features=["last_review"], days_to_today=True)
+
+
