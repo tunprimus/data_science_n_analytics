d484e5534fa2ae09106412198e485f24fa4b62a4
T Ol <31147296+tunprimus@users.noreply.github.com>
2025-03-17 17:11:50 +0100

    feat: Add better column names standardisation

--- Automating_EDA/03_automating_cleaning.py
+++ Automating_EDA/03_automating_cleaning.py
@@ -175,6 +175,50 @@ def cpu_logical_cores_count():
 
 LOGICAL_CORES = cpu_logical_cores_count()
 
+# Adapted from @georgerichardson -> https://gist.github.com/georgerichardson/db66b686b4369de9e7196a65df45fc37
+def standardise_column_names(df, remove_punct=True):
+    """
+    Standardises column names in a pandas DataFrame. By default, removes punctuation, replaces spaces with underscores and removes trailing underscores.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        DataFrame to standardise
+    remove_punct : bool, optional
+        Whether to remove punctuation from column names. Defaults to True.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame with standardised column names
+    Example
+    -------
+    >>> df = pd.DataFrame({'Column With Spaces': [1,2,3,4,5],
+                            'Column-With-Hyphens&Others/': [6,7,8,9,10],
+                            'Too    Many Spaces': [11,12,13,14,15],
+                            })
+    >>> df = standardise_column_names(df)
+    >>> print(df.columns)
+    Index(['column_with_spaces',
+            'column_with_hyphens_others',
+            'too_many_spaces'], dtype='object')
+    """
+    import pandas as pd
+    import re
+    import string
+
+    translator = str.maketrans(string.punctuation, " "*len(string.punctuation))
+    for c in df.columns:
+        c_mod = c.lower()
+        if remove_punct:
+            c_mod = c_mod.translate(translator)
+        c_mod = "_".join(c_mod.split(" "))
+        if c_mod.endswith("_") or c_mod[-1] == "_":
+            c_mod = c_mod[:-1]
+        c_mod = re.sub(r"\_+", "_", c_mod)
+        df.rename({c: c_mod}, inplace=True, axis=1)
+    return df
+
 
 ## Basic Data Wrangling
 ##*********************##
@@ -213,7 +257,10 @@ def basic_wrangling(
     """
     import pandas as pd
 
-    df.columns = df.columns.str.lower().str.strip().str.replace(" ", "_")
+    try:
+        df = standardise_column_names(df)
+    except:
+        df.columns = df.columns.str.lower().str.strip().str.replace(" ", "_")
 
     if drop_duplicates:
         if drop_duplicates_subset_name is None:
@@ -1082,6 +1129,11 @@ def missing_drop(
 # --------#
 # TESTS  #
 # --------#
+
+standardise_column_names(df_insurance)
+standardise_column_names(df_nba_salaries)
+standardise_column_names(df_airbnb)
+standardise_column_names(df_airline_satisfaction)
 
 basic_wrangling(df_insurance)
 basic_wrangling(df_nba_salaries)
